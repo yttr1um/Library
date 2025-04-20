6fe07cb928b294845b1f4b0b3f960bac07f23bbf
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-04-20 19:22:24 +0400

    Add basic form that adds to the library array

---
+++ script.js
@@ -0,0 +1,41 @@
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+const form = document.querySelector("form");
+
+let myLibrary = [];
+
+//constructor
+function Book(title, author, pages, read, id) {
+
+    if (!new.target) {
+        throw Error("You have to use the \"new\" keyword to construct a new object");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+function addBookToLibrary(library, book) {
+    library.push(book);
+}
+
+form.addEventListener("submit", (event)=>{
+    //prevents page from refreshing.
+    event.preventDefault();
+
+    const book = new Book(title.value, author.value, pages.value, read.value, crypto.randomUUID());
+    addBookToLibrary(myLibrary, book);
+    console.log(myLibrary);
+})
+
+//TODO: when submitting the form it should construct the object and then call the following function
+
+
+//TODO: use crypto.randomUUID() to generate a unique ID for every book.
+
+//TODO: loop through the array and display each book. 
